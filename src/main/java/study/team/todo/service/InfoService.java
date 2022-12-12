860f492224946dece185d9d6f9b42beb15215d42
ChoiJeonwon <jwchoi@chunjae.co.kr>
2022-12-12 16:08:07 +0900

    Service 수정

--- src/main/java/study/team/todo/service/InfoService.java
+++ src/main/java/study/team/todo/service/InfoService.java
@@ -1,5 +1,6 @@
 package study.team.todo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -8,5 +9,55 @@ import study.team.todo.info.repository.InfoRepository;
 @Service
 @AllArgsConstructor
 public class InfoService {
-    private final InfoRepository repository;
+    
+    @Autowired
+    InfoRepository repos;
+
+    public String GetTodoList() {
+        // return repos.findAll();
+        return "GetTodoList";
+    }
+
+    //Todo조회
+    //Request : 년, 월
+    //Response : Id, 년, 월, 일, 제목, 내용
+    public String GetTodoList(long id) {
+        // Blog blog = repos.findById(id).orElse(new Blog());
+        // return BlogDto.fromBlog(blog);
+        return "GetTodoList(조회조건)";
+    }
+
+    //Todo 등록
+    //Request : 년, 월, 일, 제목, 내용
+    //Response : 
+    public String PostTodo(BlogDto dto) {
+        // Blog blog = repos.save(dto.toBlog());
+        // return BlogDto.fromBlog(blog);
+        return "PostTodo(데이터)";
+    }
+
+    //Todo 수정
+    //Request : 년, 월, 일, 제목, 내용
+    //Response : 
+    public String PutTodo(BlogDto dto) {
+        // Blog blog = repos.save(dto.toBlog());
+        // return BlogDto.fromBlog(blog);
+        return "PutTodo(데이터)";
+    }
+
+    //Todo 삭제
+    //Request : Id
+    //Response : 
+    public String DeleteTodo(Long id) {
+        // repos.deleteById(id);
+        return "DeleteTodo(" + id.toString() + ")";
+    }
+
+    // @Override
+    // public Blog test() {
+    //     return repos.test();
+    // }
+
+
+
 }
